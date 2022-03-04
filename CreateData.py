5b951d29204e54cf8ea6b8b11a4a128348c103a6
FKjaeren <f.kjaer@icloud.com>
2022-03-04 16:04:59 +0100

    read data

--- CreateData.py
+++ CreateData.py
@@ -2,9 +2,17 @@
 import pandas as pd
 
 ### Import raw data
-rating_df = pd.read_csv('data/Grocery_and_Gourmet_Food.csv')
+rating_df = pd.read_csv('data/Grocery_and_Gourmet_Food.csv', names = ['item','user','rating','timestamp'])
 reviews_df = pd.read_json('data/Grocery_and_Gourmet_Food_5.json', lines=True)
 
 ### Show raw data
 rating_df.head()
 reviews_df.head()
+
+###
+reviews_df.columns
+
+### 
+rating_df['timestamp'] = pd.to_datetime(rating_df['timestamp'], origin = 'unix', unit = 's')
+
+### Time span = 2000-06-19 to 2018-10-07
