9575177b487a7aeab14ebe6d5c6b5115b59dc6c4
FKjaeren <f.kjaer@icloud.com>
2022-03-04 15:47:23 +0100

    load reviews

--- CreateData.py
+++ CreateData.py
@@ -1,3 +1,6 @@
 import pandas as pd
 
 rating_df = pd.read_csv('data/Grocery_and_Gourmet_Food.csv')
+reviews_df = pd.read_json('data/Grocery_and_Gourmet_Food_5.json', lines=True)
+
+reviews_df.head()
