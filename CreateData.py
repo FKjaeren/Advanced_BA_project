ce7098e5512ff3f5da46bfa40df1573ee0efc441
JCRL21 <62746469+JCRL21@users.noreply.github.com>
2022-04-01 15:21:15 +0200

    meta

--- CreateData.py
+++ CreateData.py
@@ -2,9 +2,10 @@
 import pandas as pd
 
 ### Import raw data
-def load_data(rating_filepath, review_filepath):
+def load_data(rating_filepath, review_filepath, meta_rating_filepath, meta_review_filepath):
     ratings_df = pd.read_csv(rating_filepath, names = ['item','user','rating','timestamp'])
     reviews_df = pd.read_json(review_filepath, lines=True)
-
-    return ratings_df, reviews_df
+    meta_rating_df = pd.read_json(rating_filepath, lines=True)
+    meta_reviews_df = pd.read_json(review_filepath, lines=True)
+    return ratings_df, reviews_df, meta_rating_df, meta_reviews_df
 
