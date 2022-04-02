a2adf9ecf83ec5f4147fb0dfb8b3394c4e8e5742
sofiebach <70587917+sofiebach@users.noreply.github.com>
2022-04-02 09:34:41 +0200

    mer meta

--- Exploratory.py
+++ Exploratory.py
@@ -2,20 +2,23 @@ from CreateData import load_data
 import seaborn as sns
 import pandas as pd
 
-def preprocess_data(ratings_df, reviews_df):
+def preprocess_data(ratings_df, reviews_df, metadata_df):
     # create timestamps
     ratings_df['timestamp'] = pd.to_datetime(ratings_df['timestamp'], origin = 'unix', unit = 's')
     reviews_df['timestamp'] = pd.to_datetime(reviews_df['unixReviewTime'], origin = 'unix', unit = 's')
 
     # drop columns in reviews
     reviews_df = reviews_df.drop(columns=['unixReviewTime','reviewTime','reviewerName','vote','image','style','verified'])
+
+    # drop columns in metadata
+    metadata_df = metadata_df.drop(columns=['imageURL','imageURLHighRes'])
     
     # drop na's and duplicates
     reviews_df = reviews_df.dropna()
     reviews_df = reviews_df.drop_duplicates(keep='first')
     ratings_df = ratings_df.drop_duplicates(keep='first')
 
-    return ratings_df, reviews_df
+    return ratings_df, reviews_df, metadata_df
 
 rating_filepath = 'data/Grocery_and_Gourmet_Food.csv'
 review_filepath = 'data/Grocery_and_Gourmet_Food_5.json'
@@ -25,7 +28,7 @@ raw_ratings, raw_reviews, raw_metadata = load_data(rating_filepath, review_filep
 
 # rating ['item', 'user', 'rating', 'timestamp']
 # review ['overall','reviewerID','asin','reviewText','summary','timestamp]
-ratings_df, reviews_df = preprocess_data(raw_ratings, raw_reviews)
+ratings_df, reviews_df, metadata_df = preprocess_data(raw_ratings, raw_reviews, raw_metadata)
 
 
 
