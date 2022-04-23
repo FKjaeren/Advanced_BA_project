fe20ead7e5828c9cc4cdd5bccf937fbb100a9cfe
FKjaeren <f.kjaer@icloud.com>
2022-04-23 13:34:21 +0200

    .

--- Exploratory.py
+++ Exploratory.py
@@ -19,26 +19,31 @@ df_category = df.copy(deep=True)
 df_category['category'] = df_category['category'].apply(lambda row: get_category(row, categories))
 df_category = df_category[df_category['category'] != '']
 
-plt.figure()
+f1 = plt.figure(1)
 sns.countplot(x="category", data=df_category, order=categories)
 plt.xticks(rotation=90)
 plt.tight_layout()
-plt.show()
+f1.show()
 
 # Number of ratings in each category
 df_num_ratings = df[['category','num_ratings']].groupby(by=["category"]).sum(["num_ratings"])
 df_num_ratings = df_num_ratings['num_ratings'].sort_values(ascending=False).reset_index()
 top = 20
 
-plt.figure()
+f2 = plt.figure(2)
 sns.barplot(x="category", y="num_ratings", data=df_num_ratings[0:top])
 plt.xticks(rotation=90)
 plt.tight_layout()
-plt.show()
+f2.show()
 
-
-
-
+# Variance of avgerage ratings in each category 
+top = 20
+categories_largest_var = df.groupby(['category']).var().sort_values(by='avg_rating',ascending=False).index[0:top].to_list()
+f3 = plt.figure(3)
+sns.boxplot(x = 'category', y = 'avg_rating', data = df[df['category'].isin(categories_largest_var)], order=categories_largest_var)
+plt.xticks(rotation=90)
+plt.tight_layout()
+f3.show()
 
 
 sns.pairplot(df)
