bc2d666ae113852a23ac36c16ae8ad3ca8edcc25
sofiebach <70587917+sofiebach@users.noreply.github.com>
2022-04-24 14:53:27 +0200

    grid search num topics + wordcloud

--- train_lda.py
+++ train_lda.py
@@ -4,7 +4,10 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
 import numpy as np
 import pickle
+from sklearn.model_selection import GridSearchCV
 from datetime import date
+import wordcloud
+import matplotlib.pyplot as plt
 
 # set random seed
 np.random.seed(42)
@@ -18,7 +21,7 @@ def get_len(text):
     else:
         return len(text)
 
-def train_lda(df, n_topics, text):
+def train_lda(df, n_topics, ld, text):
     df['message_len'] = df[text].apply(get_len)
     count_vect = CountVectorizer()
     #bow_counts = count_vect.fit_transform(df.dropna(subset=[text])[text].values)
@@ -27,7 +30,8 @@ def train_lda(df, n_topics, text):
 
     lda = LatentDirichletAllocation(n_components=n_topics, max_iter=5,
                                 learning_method='online',
-                                learning_offset=50.)
+                                learning_offset=50.,
+                                learning_decay=ld)
 
     X_len = df['message_len'].values
     print(X_len)
@@ -47,6 +51,32 @@ def print_top_words(model, feature_names, n_top_words):
             print("{:.3f}".format(topic[i] / norm[topic_idx][0]) 
                   + '\t' + feature_names[i])
 
+def visualize_topics(lda, count_vect, terms_count):
+    terms = count_vect.get_feature_names()
+    for idx, topic in enumerate(lda.components_):
+        title = 'Topic ' + str(idx+1)
+        abs_topic = abs(topic)
+        topic_terms = [[terms[i],topic[i]] for i in abs_topic.argsort()[:-terms_count-1:-1]]
+        topic_terms_sorted = [[terms[i], topic[i]] for i in abs_topic.argsort()[:-terms_count - 1:-1]]
+        topic_words = []
+        for i in range(terms_count):
+            topic_words.append(topic_terms_sorted[i][0])
+            print(','.join( word for word in topic_words))
+            print("")
+            dict_word_frequency = {}
+        for i in range(terms_count):
+            dict_word_frequency[topic_terms_sorted[i][0]] = topic_terms_sorted[i][1]
+            wcloud = wordcloud.WordCloud(background_color="white",mask=None, max_words=100,\
+            max_font_size=60,min_font_size=10,prefer_horizontal=0.9,
+            contour_width=3,contour_color='black')
+            wcloud.generate_from_frequencies(dict_word_frequency)
+        plt.imshow(wcloud, interpolation='bilinear')
+        plt.axis("off")
+        plt.title(title, fontsize=20)
+        # plt.savefig("Topic#"+str(idx+1), format="png")
+        plt.show()
+    return
+
 #%% RUN LDA
 
 # get data
@@ -57,8 +87,37 @@ else:
     path = 'data/metadata_df_preprocessed.csv'
 df = pd.read_csv(path)
 
-n_topics = 10 # number of topics
-X_lda, lda, count_vect= train_lda(df = df, n_topics=n_topics, text = 'description')
+# Options to try with our LDA
+# Beware it will try *all* of the combinations, so it'll take ages
+search_params = {'n_components': [1, 5], 'learning_decay': [.5, .7]}
+
+# Set up LDA with the options we'll keep static
+model = LatentDirichletAllocation(learning_method='online',
+                                  max_iter=5,
+                                  random_state=0)
+
+# Try all of the options
+gridsearch = GridSearchCV(model,
+                          param_grid=search_params,
+                          n_jobs=-1,
+                          verbose=1)
+count_vect = CountVectorizer()
+cv_matrix = count_vect.fit_transform(df['description'].values)
+gridsearch.fit(cv_matrix)
+
+## Save the best model
+best_lda = gridsearch.best_estimator_
+
+# What did we find?
+print("Best Model's Params: ", gridsearch.best_params_)
+
+# Train LDA with best params
+n_topics = gridsearch.best_params_['n_components']
+learning_decay = gridsearch.best_params_['learning_decay']
+X_lda, lda, count_vect = train_lda(df, n_topics, learning_decay, 'description')
+
+# Visualize topics as wordclouds
+visualize_topics(lda, count_vect, 25)
 
 # print top words from lda model 
 print("\nTopics in LDA model:")
